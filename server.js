97e89fd0a1e7326249e4a3dacb4715386c1d9853
rutvi-87 <rutvisheta87@gmail.com>
2025-03-28 21:04:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -241,6 +241,8 @@ app.post('/check-domain', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
+
 });
